49c6883689ae552bfdc5fd261e51838f08999c47
Indy Ray <iray@telltale.com>
2021-01-11 14:26:52 -0800

    Adding link to SSM Instance connect.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const fetch = require('node-fetch');
 async function get_info() {
     let instanceId = ((await fetch("http://169.254.169.254/latest/meta-data/instance-id")).text());
     core.info(`Instance Id: ${instanceId}`);
+    core.info(`Connect: https://console.aws.amazon.com/systems-manager/session-manager/${instanceId}`);
 }
 
 try {
